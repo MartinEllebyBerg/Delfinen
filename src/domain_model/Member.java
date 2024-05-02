9d925e40f4d3b1caacd73e620c5b012ae8779377
MetteMunch <meha0005@stud.kea.dk>
2024-05-02 10:17:18 +0200

    calculate age og rate

--- src/domain_model/Member.java
+++ src/domain_model/Member.java
@@ -1,6 +1,7 @@
 package domain_model;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Member {
 
@@ -11,6 +12,8 @@ public class Member {
     private boolean memberActive;
     private static int nextMemberID = 1; //statisk attribut til at sammentælle memberID og sørge for unikt nummer.
     private int memberID;
+    private int yearsOfAge;
+    private boolean paymentRegistered;
 
     //CONSTRUCTOR
     public Member(String firstName, String lastName, LocalDate birthday, boolean memberActive) {
@@ -19,13 +22,31 @@ public class Member {
         this.birthday = birthday;
         this.memberActive = memberActive;
         memberID = nextMemberID++;
+        paymentRegistered = false;
     }
 
     //METHODS
-    public double calculateMembershipRate() {
-        //Her kommer kode
-        return 0;
+
+    public void calculateYearOfMember() {
+        LocalDate presentTime = LocalDate.now();
+        Period calculatedPeriode = Period.between(birthday, presentTime);
+        yearsOfAge = calculatedPeriode.getYears();
     }
+
+    public void calculateMembershipRate() {
+        double rate;
+        if (memberActive == false) {
+            rate = 500.00;
+        } else if (yearsOfAge < 18) {
+            rate = 1000.00;
+        } else if (yearsOfAge >= 18 && yearsOfAge < 60) {
+            rate = 1600.00;
+        } else {
+            rate = 1200.00;
+        }
+
+    }
+
 
     public String getFirstName() {
         return firstName;
